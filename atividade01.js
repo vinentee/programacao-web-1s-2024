60560af8ce1d42c6bd899b1c9921909a3c29bee5
Gabriel <gabrielvinente144.gv@gmail.com>
2024-03-07 12:22:32 -0300

    Update atividade01.js

--- atividade01.js
+++ atividade01.js
@@ -1,8 +1,20 @@
-// Operação Soma
+//Declaração de variáveis
 var a = 6;
 var b = 8;
 var c = 11;
 
+// Operação Soma
 var d = a + b + c;
-
 console.log(`A soma de ${a}, ${b} e ${c} é ${d}`)
+
+// Operação Subtração
+var e = c - b;
+console.log(`A subtração de ${c} e ${b} é ${e}`)
+
+// Operação Multiplicação
+var f = a * b * c;
+console.log(`A multiplicação de ${a}, ${b} e ${c} é ${f}`)
+
+// Operação Divisão
+var g = b / a;
+console.log(`A divisão de ${b} e ${a} é ${g}`)
