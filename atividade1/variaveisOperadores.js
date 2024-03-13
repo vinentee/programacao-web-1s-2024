a62cc019888a877fd80e85d9bd316706788e6efb
Gabriel <gabrielvinente144.gv@gmail.com>
2024-03-13 09:37:35 -0300

    Create variaveisOperadores.js

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,27 @@
+// Operação de soma (+)
+let a = 2
+let b = 3
+let c = a + b
+
+console.log(`Resultado da + entre ${a} e ${b} = ${c}`)
+
+// Operação de subtração (-)
+let d = 6
+let e = 8
+let f = d - e
+
+console.log(`Resultado da - entre ${d} e ${e} = ${f}`)
+
+// Operação de multiplicação (*)
+let g = 12
+let h = 12
+let i = g * h
+
+console.log(`Resultado da * entre ${g} e ${h} = ${i}`)
+
+// Operação de divisão (/)
+let k = 125
+let l = 25
+let m = k / l
+
+console.log(`Resultado da / entre ${k} e ${l} = ${m}`)
