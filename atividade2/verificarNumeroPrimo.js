27b296ba30bbdb45a52a75220fd67c73e507f427
Gabriel <gabrielvinente144.gv@gmail.com>
2024-03-13 19:57:53 -0300

    Create verificarNumeroPrimo.js

---
+++ atividade2/verificarNumeroPrimo.js
@@ -0,0 +1,32 @@
+function verificarNumeroPrimo(n) {
+    if (n <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(verificarNumeroPrimo(0));
+console.log(verificarNumeroPrimo(1));
+console.log(verificarNumeroPrimo(2));
+console.log(verificarNumeroPrimo(3));
+console.log(verificarNumeroPrimo(7));
+console.log(verificarNumeroPrimo(83));
+console.log(verificarNumeroPrimo(100));
+console.log(verificarNumeroPrimo(991));
+console.log(verificarNumeroPrimo(104729));
+console.log(verificarNumeroPrimo(14348907));
+
+// var a = 14348907
+
+// if (verificarNumeroPrimo(a)) {
+//     console.log('O número é primo')
+// } else {
+//     console.log('O número não é primo')
+// }
