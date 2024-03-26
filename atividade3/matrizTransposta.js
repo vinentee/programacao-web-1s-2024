ae092ea6871ef930605c1b83aa57eb992ac82870
Gabriel <gabrielvinente144.gv@gmail.com>
2024-03-26 17:47:56 -0300

    Create matrizTransposta.js

---
+++ atividade3/matrizTransposta.js
@@ -0,0 +1,32 @@
+function transporMatriz(A) {
+
+    console.log('Matriz Original:');
+    imprimirMatriz(A);
+
+    const transposta = [];
+
+    for (let i = 0; i < A[0].length; i++) {
+        transposta[i] = [];
+        for (let j = 0; j < A.length; j++) {
+            transposta[i][j] = A[j][i];
+        }
+    }
+
+    console.log('Matriz Transposta:');
+    imprimirMatriz(transposta);
+}
+
+function imprimirMatriz(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+        console.log(matriz[i].join('\t'));
+    }
+    console.log('');
+}
+
+const matrizOriginal = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+transporMatriz(matrizOriginal);
